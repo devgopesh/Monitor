fb7a471c5d352e3f37b93ffa450f995a6f5ebf0f
mnprtpsingh <mnprtpsingh@gmail.com>
2017-02-04 23:26:02 +0530

    Another layout added

--- app/src/main/java/com/example/android/monitor/SummaryActivity.java
+++ app/src/main/java/com/example/android/monitor/SummaryActivity.java
@@ -3,7 +3,7 @@ package com.example.android.monitor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class summary extends AppCompatActivity {
+public class SummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
