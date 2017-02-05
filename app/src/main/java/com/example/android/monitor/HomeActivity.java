1bba1434f2b70caf5a09f69119728d9dc4d20d0e
mnprtpsingh <mnprtpsingh@gmail.com>
2017-02-05 10:28:04 +0530

    Activites Linked

--- app/src/main/java/com/example/android/monitor/HomeActivity.java
+++ app/src/main/java/com/example/android/monitor/HomeActivity.java
@@ -1,13 +1,25 @@
 package com.example.android.monitor;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    MainActivity home = new MainActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void submit(View view) {
+        SharedPreferences.Editor editor = home.sharedPreferences.edit();
+        editor.putString("name", home.name.getText().toString());
+        editor.putString("branch", home.branch.getText().toString());
+        editor.putString("semester", home.semester.getText().toString());
+        editor.commit();
     }
 }
--- app/src/main/java/com/example/android/monitor/MainActivity.java
+++ app/src/main/java/com/example/android/monitor/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.android.monitor;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name = (EditText) findViewById(R.id.name);
+    EditText branch = (EditText)findViewById(R.id.branch);
+    EditText semester = (EditText)findViewById(R.id.semester);
+
+    SharedPreferences sharedPreferences = getSharedPreferences("MyData", MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void monitor(View view) {
+        Intent i = new Intent(this, MonitorActivity.class);
+        startActivity(i);
+    }
+
+    public void summary(View view) {
+        Intent i = new Intent(this, SummaryActivity.class);
+        startActivity(i);
+    }
+
+    public void profile(View view) {
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+
+    public void settings(View view) {
+        Intent i = new Intent(this, MonitorActivity.class);
+        startActivity(i);
     }
 }
